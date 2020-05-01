5b90364e29f0afd7f8b8e19bc13e4f08e8e25461
Tomasz Janowski <janaz9@gmail.com>
2020-05-02 00:35:22 +1000

    print version

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 
 app.get('/inspect', (_req, res) => {
-  res.json({hello: 'world'})
+  res.json({
+    hello: 'world',
+    version: process.env.VERSION
+  })
 })
 
 module.exports = app
