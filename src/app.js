646cefc0ff67b13da51dfed36ae01767cfd56fe3
Tomasz Janowski <janaz9@gmail.com>
2020-05-02 00:45:53 +1000

    More features

--- src/app.js
+++ src/app.js
@@ -1,12 +1,66 @@
 const express = require('express')
-
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const compression = require('compression')
 const app = express()
 
-app.get('/inspect', (_req, res) => {
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '..', 'views'))
+app.set('trust proxy', 2)
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/inspect', (req, res) => {
+  res.json({
+    body: req.body,
+    cookies: req.cookies,
+    fresh: req.fresh,
+    hostname: req.hostname,
+    method: req.method,
+    params: req.params,
+    protocol: req.protocol,
+    query: req.query,
+    secure: req.secure,
+    signedCookies: req.signedCookies,
+    stale: req.stale,
+    subdomains: req.subdomains,
+    xhr: req.xhr,
+    ip: req.ip,
+    ips: req.ips,
+    originalUrl: req.originalUrl,
+    baseUrl: req.baseUrl,
+    path: req.path,
+    url: req.url,
+    xForwardedFor: req.get('x-forwarded-for'),
+    version: process.env.VERSION,
+    lambdaRequestId: req.header('x-aws-lambda-request-id'),
+  })
+})
+
+app.all('/echo', (req, res) => {
   res.json({
     hello: 'world',
     version: process.env.VERSION
   })
+})
+
+app.use("/static", compression({}), express.static(path.join(__dirname, '..', 'public')))
+
+app.get('/render', (_, res) => {
+  res.render('page')
+})
+
+app.get('/cookies', (_, res) => {
+  res.cookie('chocolate', '10')
+  res.cookie('peanut_butter', '20')
+  res.cookie('cinnamon', '30')
+  res.end()
+})
+
+app.get('/user/:id', (req, res) => {
+  res.json({name: 'John', id: req.params.id})
 })
 
 module.exports = app
