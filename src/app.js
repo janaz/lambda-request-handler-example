5b050a4dc9187e0727d7215e05dae3e1dedd80e0
Tomasz Janowski <janaz9@gmail.com>
2020-05-02 15:14:57 +1000

    Allow all origins

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '..', 'views'))
 app.set('trust proxy', 2)
 
-app.use(cors())
+app.use(cors({origin: true}))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
