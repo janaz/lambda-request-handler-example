a0643420c9fc29d33e26f9f5f5c9ddc3178397fb
Tomasz Janowski <janaz9@gmail.com>
2020-05-02 19:16:50 +1000

    Stop logging

--- src/handler.js
+++ src/handler.js
@@ -1,12 +1,6 @@
 const lambdaRequestHandler = require('lambda-request-handler')
 const app = require('./app')
 
-const h = lambdaRequestHandler(app);
-const handler = async (ev) => {
-  console.log("REQ", JSON.stringify(ev))
-  const res = await h(ev);
-  console.log("RES", JSON.stringify(res))
-  return res;
-}
+const handler = lambdaRequestHandler(app)
 
 module.exports = { handler }
