d761af91368f93221b97ec5ed4a184fc1bdb04a6
Tomasz Janowski <janaz9@gmail.com>
2020-05-02 21:02:47 +1000

    Log for a little bit more
    This reverts commit a0643420c9fc29d33e26f9f5f5c9ddc3178397fb.

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,12 @@
 const lambdaRequestHandler = require('lambda-request-handler')
 const app = require('./app')
 
-const handler = lambdaRequestHandler(app)
+const h = lambdaRequestHandler(app);
+const handler = async (ev) => {
+  console.log("REQ", JSON.stringify(ev))
+  const res = await h(ev);
+  console.log("RES", JSON.stringify(res))
+  return res;
+}
 
 module.exports = { handler }
