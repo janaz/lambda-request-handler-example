4e09de3f249651f449ee76cabed3dde65fee8e05
Tomasz Janowski <janaz9@gmail.com>
2020-05-02 14:56:26 +1000

    Add CORS support

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const compression = require('compression')
+const cors = require('cors')
+
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '..', 'views'))
 app.set('trust proxy', 2)
 
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
